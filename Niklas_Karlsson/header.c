085379621257be6e7d3beaeaae6f6b9eec51ed49
Niklas Karlsson <schlook@users.noreply.github.com>
2021-10-05 17:14:26 +0200

    Update header.c

--- Niklas_Karlsson/header.c
+++ Niklas_Karlsson/header.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+// Lite att leka med
+
+// Denna lever genom hela programmet
+// Kan inte ha initiering
+extern int i;
+
 int main()
 {
+    // Denna lever endast i main();
+    int i = 3;
+    printf("%d", i);
 }
+
+void fun(void)
+{
+  printf("%d", i);
+}
+
