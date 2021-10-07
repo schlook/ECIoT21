7bed3726662494da9c4bd07ba1712bd6dc2640f9
IvanaMladenovic89 <you@example.com>
2021-10-07 14:52:31 +0200

    senaste

--- Ivana_Mladenovic/Embedded C/Övningar v40/Övning10v40.c
+++ Ivana_Mladenovic/Embedded C/Övningar v40/Övning10v40.c
@@ -4,11 +4,13 @@ int main(void)
     int rader;
         printf("Ange önskat antal rader: ");
         scanf("%d", &rader);
+    //för i lika med 0; i mindre eller lika med antal rader; öka med 1
     for (int i = 1; i <=rader; i++)
     {
+        //för j lika med 1; j mindre eller lika med i; j öka med 1
         for (int j = 1; j <= i; j++)
         {
-            printf("%d", j);
+            printf("%d ", j);
         }
         printf("\n");
     }
