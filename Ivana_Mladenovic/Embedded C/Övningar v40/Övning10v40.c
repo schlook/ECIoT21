69ed69eaa1819f9690394e5d23188c4e15a911a1
IvanaMladenovic89 <you@example.com>
2021-10-07 14:47:47 +0200

    sista

--- Ivana_Mladenovic/Embedded C/Övningar v40/Övning10v40.c
+++ Ivana_Mladenovic/Embedded C/Övningar v40/Övning10v40.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 int main(void)
 {
-    
+    int rader;
+        printf("Ange önskat antal rader: ");
+        scanf("%d", &rader);
+    for (int i = 1; i <=rader; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
 }
