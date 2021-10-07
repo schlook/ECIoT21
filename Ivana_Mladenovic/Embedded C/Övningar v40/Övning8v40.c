7e3605874786eb656eee20c99b79701f56f0eb69
IvanaMladenovic89 <you@example.com>
2021-10-07 14:25:40 +0200

    senaste

--- Ivana_Mladenovic/Embedded C/Övningar v40/Övning8v40.c
+++ Ivana_Mladenovic/Embedded C/Övningar v40/Övning8v40.c
@@ -2,7 +2,7 @@
 int main(void)
 {
     char val;
-    double tal1, tal2;
+    double tal1, tal2, summa, differens, produkt, kvot;
     printf("Vad vill du göra? Gör ditt val: +, -, * eller /");
     scanf("%c", &val);
     printf("Skriv in 2 tal: \n");
@@ -10,18 +10,18 @@ int main(void)
     switch (val)
     {
     case '+':
-        double summa=tal1+tal2;
+        summa=tal1+tal2;
         printf("Summan är %.2lf", summa);
         break;
     case '-':
-        double differens=tal1-tal2;
+        differens=tal1-tal2;
         printf("Differensen är %.2lf", differens);
     case '*':
-        double produkt=tal1*tal2;
+        produkt=tal1*tal2;
         printf("Produkten är %.2lf", produkt);
     //case '/':
     case 47:printf("Summan är %n", summa);
-        double kvot=tal1/tal2;
+        kvot=tal1/tal2;
         printf("Kvoten är %.2lf", kvot);
     default:
         printf("Ogiltigt val!");
