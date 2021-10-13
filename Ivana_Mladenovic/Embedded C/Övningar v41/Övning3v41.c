ccbcc0df0fbceaf85848f11ddf156bbc9da201ff
IvanaMladenovic89 <you@example.com>
2021-10-13 18:48:21 +0200

    commit

---
+++ Ivana_Mladenovic/Embedded C/Övningar v41/Övning3v41.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+float kalkyleraSumma(float nummer[]);
+int main(void)
+{
+    float nummer[]={3.5, 45, -8, 22.4, 6.9};
+    float resultat;
+    resultat=kalkyleraSumma(nummer);
+    return 0;
+}
+float kalkyleraSumma(float nummer[])
+{
+    float summa;
+    for (int i = 0; i < 6; i++)
+    {
+        summa +=nummer[i];
+    }
+    printf("Resultat är: %f",summa);
+    printf("=%.2f",summa);
+}
