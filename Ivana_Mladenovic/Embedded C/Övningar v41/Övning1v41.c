3d656c43237d746634d08d2214992ccbc4c8d781
IvanaMladenovic89 <you@example.com>
2021-10-13 15:49:21 +0200

    comit

--- Ivana_Mladenovic/Embedded C/Övningar v41/Övning1v41.c
+++ Ivana_Mladenovic/Embedded C/Övningar v41/Övning1v41.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main(void)
 {   //Om jag lägger in '\0' som avslut i arrayen får jag varning
-    char mychar[]={'B', 'a', 'b', 'y', 's', 'h', 'a', 'r', 'k'};
-    for(int i=0; i<=(sizeof(char)/sizeof(mychar[0])); i++)
+    char mychar[]={'B', 'a', 'b', 'y',' ', 's', 'h', 'a', 'r', 'k','\0'};
+    for(int i=0; mychar[i]!='\0'; i++)
     {
-        //Varför får jag bara 'Ba' i utskrift??
-    printf("%c", mychar[i]);
+        printf("%c", mychar[i]);
     }
+
     return 0;
 }
