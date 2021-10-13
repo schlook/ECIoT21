63a8d74bbdd4d2b7dc9d774e3b19dc39f0b1bffa
IvanaMladenovic89 <you@example.com>
2021-10-13 19:53:48 +0200

    com

--- Ivana_Mladenovic/Embedded C/Övningar v41/Övning4v41.c
+++ Ivana_Mladenovic/Embedded C/Övningar v41/Övning4v41.c
@@ -2,7 +2,8 @@
 #define BUF 6
 void utskrift(void);
 float kalkylSumma(float inmatning[]);
-float inmatning[6]={};
+//arrayen får en storlek som är lika med BUF
+float inmatning[BUF]={};
 int main(void)
 {
     utskrift();
@@ -14,14 +15,15 @@ int main(void)
 void utskrift(void)
 {
     for (int i = 0; i < BUF; i++)
-    {
+    {   //i+1 öka indexet med 1 för att lägga inmatningen en efter en i följd 1-6
         printf("Ange %d värde(decimaler separeras med punkt '.'): ",i+1 );
         scanf("%f", &inmatning[i]);
     }
 
 }
 float kalkylSumma(float inmatnng[])
-{
+{   //summa får värde 0 för att vara på dn säkra sidan 
+    //att den inte ska innehålla något kvar från förra inmatningen
     float summa=0.0;
     for (int i = 0; i < BUF; i++)
     {
