bb7f23cdf30ccc9be6a1bb569ab1c16105769fc4
IvanaMladenovic89 <you@example.com>
2021-10-13 15:50:57 +0200

    commit

--- Ivana_Mladenovic/Embedded C/Övningar v41/Övning1v41.c
+++ Ivana_Mladenovic/Embedded C/Övningar v41/Övning1v41.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(void)
-{   //Om jag lägger in '\0' som avslut i arrayen får jag varning
+{   
     char mychar[]={'B', 'a', 'b', 'y',' ', 's', 'h', 'a', 'r', 'k','\0'};
+    //så länge strängen inte är slut (\0) skriv ut
     for(int i=0; mychar[i]!='\0'; i++)
     {
         printf("%c", mychar[i]);
