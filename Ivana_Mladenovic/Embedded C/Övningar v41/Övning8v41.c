5c84ae6e28e3a748971257f0a8c533902fa99db3
IvanaMladenovic89 <you@example.com>
2021-10-14 18:12:48 +0200

    com

--- Ivana_Mladenovic/Embedded C/Övningar v41/Övning8v41.c
+++ Ivana_Mladenovic/Embedded C/Övningar v41/Övning8v41.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 int main(void)
 {
-    //h„r sparas inmatning fr†n anv„ndaren
+    //hÃ¤r sparas inmatning frÃ¥n anvÃ¤ndaren
     int antal=0;
-    printf("Ange antal f„lt: ");
+    printf("Ange antal fÃ¤lt: ");
     scanf("%d", &antal);
-    //sparad inmatning anv„nds som storlek p† f„ltvariabel
+    //sparad inmatning anvÃ¤nds som storlek pÃ¥ fÃ¤ltvariabel
     int felt[antal];
     for (int i = 0; i < antal; i++)
     {
-        //F”r varje nytt varv f†r anv„ndaren mata in nytt nr
+        //FÃ¶r varje nytt varv fÃ¥r anvÃ¤ndaren mata in nytt nr
         printf("Ange ett nummer: ");
-        //nummret sparas i f„ltvariabeln
+        //nummret sparas i fÃ¤ltvariabeln
         scanf("%d", &felt[i]);
     }
-    printf("Nedan f†r du dina siffror i omv„nd ordning:\n ");
-    //i „r lika med 'antal'-1(m†ste skrivas -1 f”r att r„kna bort \0 sista tomma platsen 
-    //annars r„knas „ven den med och skrivs ut med siffran '32767'), 
-    //i „r st”rre eller lika med 0, i minskar med 1
+    printf("Nedan fÃ¥r du dina siffror i omvÃ¤nd ordning:\n ");
+    //i Ã¤r lika med 'antal'-1(mÃ¥ste skrivas -1 fÃ¶r att rÃ¤kna bort \0 sista tomma platsen 
+    //annars rÃ¤knas Ã¤ven den med och skrivs ut med siffran '32767'), 
+    //i Ã¤r stÃ¶rre eller lika med 0, i minskar med 1
     for (int i = antal-1; i >= 0; i--)
     {
         printf("%d, ", felt[i]); 
