aa82963912470f24eeb5b655c6416c0a97575af6
MattiasICHIlindgren <mattiaslindgren367@gmail.com>
2021-10-14 11:57:39 +0200

    test

---
+++ MAttias_Lindgren/Övning v 41/övning1.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+
+char arrays [] ="Baby shark!"; 
+
+int main ()
+{
+
+    while (1)
+    {
+        printf("%s\n" , arrays) ;
+    }
+    
+}
---
+++ MAttias_Lindgren/Övning v 41/övning2.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+// function1
+void minfunction1 (char a []); 
+//function2
+void minfunction2 (char a []);
+//global variabel 
+char text; 
+
+int main ()
+
+{
+    //inmatnings text 
+printf("skriv in din text\n"); 
+scanf("%s\n" &text); 
+
+
+}
