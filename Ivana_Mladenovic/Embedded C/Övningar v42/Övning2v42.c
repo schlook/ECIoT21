1f8be56ea9d95dba758b756e5d117ece3600b42c
IvanaMladenovic89 <you@example.com>
2021-10-21 09:46:40 +0200

    commit

--- Ivana_Mladenovic/Embedded C/Övningar v42/Övning2v42.c
+++ Ivana_Mladenovic/Embedded C/Övningar v42/Övning2v42.c
@@ -7,20 +7,30 @@ int main(void)
 }
 void klassLista(void)
 {
-    char f_namn[][256]={"Ivana", "Elin", "William"};
+    char f_namn[][256]={"Ivana", "Elin", "William", "John"};
     char e_namn[][256]={"Mladenovic", "Sundbom", "Hagelin"};
+    //kollar hur många element det finns i första fältet 'a'
+    //resultatet sparas i variabeln freknare
     size_t freknare=(sizeof(f_namn)/sizeof(f_namn[0]));
+    //kollar hur många element det finns i fältet 'b'
     size_t ereknare=(sizeof(e_namn)/sizeof(e_namn[0]));
     size_t total;
+    //om antal element i v.freknare är olika av antal element i v.ereknare
     if (freknare != ereknare)
     {
-        printf("Antal förnamn och efternamn är inte lika!");
+        //felmeddelande
+        printf("Antal förnamn och efternamn är inte lika!\n");
+        //skriver hur många antal element finns i respektive fält
         printf("Det finns %d förnamn och %d efternamn i listan!", freknare, ereknare);
+        return;
     }
     else
     {
+        //variabel total får ett värde av antingen freknare eller ereknare då
+        //de är lika stora, sedan används total i for-loopen nedan för utskrift
         total=freknare;
     }
+    //skriver ut hela listan via for-loopen
     printf("I klasslistan finns: \n");
     for (size_t i = 0; i < total ; i++)
     {
