4539aaf2c692a1d1ab90d4c295031e30ca799c49
IvanaMladenovic89 <you@example.com>
2021-10-27 10:56:08 +0200

    senaste

---
+++ Ivana_Mladenovic/Embedded C/Övningar v42/Palindrom.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main(void)
+{
+    char text[200];
+    int start, mitt, slut, lengd=0;
+    printf("Skriv ett ord du tror är palindrom: \n");
+    scanf("%s", &text);//FUNKAR INTE, FRYSER EFTER INPUT!!
+    while(text[lengd] != '\0');
+    {
+        //räknar +1 för varje varv
+        lengd++;
+    }
+    //initierar slut med längden av char -1 pga sista tomma elementet \0
+    slut= lengd-1;
+    //mitt får mittvärde, hela längden delat på 2
+    mitt= lengd/2;
+    for (start = 0; start < mitt; start++)
+    {
+        if(text[start] =! text[slut])
+        {
+            printf("Det är inte en palindrom!\n");
+            break;
+        }
+        slut--;
+    }
+    if(start == mitt)
+    {
+        printf("Der är ett palindrom!\n");
+    }
+    
+    return 0;
+}
