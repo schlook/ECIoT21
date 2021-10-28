24b39b076f27ae599158803b6e243f9fa4c0cd0e
IvanaMladenovic89 <you@example.com>
2021-10-28 11:45:55 +0200

    ö5v43

---
+++ Ivana_Mladenovic/Embedded C/Övningar v43/Övning5v43.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//enum får namn 'färg'
+enum farg{spader = 0, ruter = 10, hjarter = 20, klever = 3};
+
+int main(void)
+{
+    //skapar enum variabeln 'kort'
+    enum farg kort;
+    //använder mig av sizof för att få reda på storleken av enum variabeln
+    int storlek=sizeof(kort);
+    printf("Storlek på enum variabeln 'kort' är %d bytes", storlek);
+    return 0;
+}
