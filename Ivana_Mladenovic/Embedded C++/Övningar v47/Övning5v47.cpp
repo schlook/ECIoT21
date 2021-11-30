bb843c2699053be07bdc8099219d0357d56c72d7
IvanaMladenovic89 <you@example.com>
2021-11-30 09:54:31 +0100

    5

--- Ivana_Mladenovic/Embedded C++/Övningar v47/Övning5v47.cpp
+++ Ivana_Mladenovic/Embedded C++/Övningar v47/Övning5v47.cpp
@@ -4,20 +4,29 @@
 class Telefonbok
 {
     public:
-        std::string namn[50];
-        int nummer[10];
-        void läggTill(std::string n, int nr){int i; this->namn[i]=n; nummer[i]=nr;}
+        std::string namn;
+        std::string nummer;
+        void läggTill(std::string n, std::string  nr){this->namn=n; this->nummer=nr;}
         void kollaUpp(void)
         {
-            while(namn=='\0' && nummer=='\0')
-            {
-
-            }
+            std::cout<<"TELEFONLISTA:"<<std::endl<<std::endl;
+            std::cout<<"Namn: "<<this->namn
+            <<" tel: "<<this->nummer<<std::endl;
         }
-        void taBort();
+        void taBort()
+        {
+            //rensar namn
+            namn.clear();
+            //sätter värdet till-1
+            nummer=-1;
+        }
 };
 int main(void)
 {
-
+    Telefonbok lista;
+    lista.läggTill("Ivana", "0700224496");
+    lista.kollaUpp();
+    lista.taBort();
+    lista.kollaUpp();
     return 0;
 }
