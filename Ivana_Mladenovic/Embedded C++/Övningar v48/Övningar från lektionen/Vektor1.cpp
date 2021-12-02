89ada98aee332abcad92faeacb84382e060b30cc
IvanaMladenovic89 <you@example.com>
2021-12-02 09:39:26 +0100

    senaste

---
+++ Ivana_Mladenovic/Embedded C++/Övningar v48/Övningar från lektionen/Vektor1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    vector<string> minVektor{};
+    for (int i = 0; i < 5; i++)
+    {
+        /* code */
+    }
+    
+    return 0;
+}
