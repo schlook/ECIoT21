725a0f16be05597265470fd0d3e4768ae58dd480
IvanaMladenovic89 <you@example.com>
2021-12-27 17:00:48 +0100

    ö3

--- Ivana_Mladenovic/Python/Övningar v51/Övning3v51.py
+++ Ivana_Mladenovic/Python/Övningar v51/Övning3v51.py
@@ -1,6 +1,6 @@
 antal=int(input("Ange hur många siffror som ska anges: "))
 summa=0
-# med funktionen range() loopas koden igenom ett antal ggr, här det värdet i variabel antal
+# med funktionen range() loopas koden igenom ett antal ggr, här det värde i variabel antal
 for x in range(antal):
     tal=int(input("Ange ett heltal: "))
     # för varje varv adderas nytt tal till summan
