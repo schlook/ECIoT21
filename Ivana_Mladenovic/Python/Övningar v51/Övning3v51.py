48c60ade18745220654fe4d8fe60fd4ed37f891f
IvanaMladenovic89 <you@example.com>
2021-12-27 16:55:29 +0100

    com

---
+++ Ivana_Mladenovic/Python/Övningar v51/Övning3v51.py
@@ -0,0 +1,10 @@
+antal=int(input("Ange hur många siffror som ska anges: "))
+summa=0
+# med funktionen range() loopas koden igenom ett antal ggr, här det värdet i variabel antal
+for x in range(antal):
+    tal=int(input("Ange ett heltal: "))
+    # för varje varv adderas nytt tal till summan
+    summa=summa+tal
+medel=summa/antal
+# printar ut medelvärdet
+print("Medeltalet är: ",medel)
