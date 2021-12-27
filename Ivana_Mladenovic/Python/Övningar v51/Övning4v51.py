04f59e8f5c8c79df84444706ef66a1949f1dc8b1
IvanaMladenovic89 <you@example.com>
2021-12-27 18:00:44 +0100

    ö4

---
+++ Ivana_Mladenovic/Python/Övningar v51/Övning4v51.py
@@ -0,0 +1,6 @@
+r=float(input("Mata in cirkelns radie: "))
+import math # för pi
+omkrets=float(2*math.pi*r)# beräknar omkrets
+area=float(math.pi*r*r)# beräknar area
+print("Cirkelns omkrets är: ",omkrets)
+print("Cirkelns area är: ",area)
