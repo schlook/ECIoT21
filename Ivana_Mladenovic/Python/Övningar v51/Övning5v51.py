df46409a83d51e58be2edaaece65d8d01b5bf8f4
IvanaMladenovic89 <you@example.com>
2021-12-28 20:33:05 +0100

    ö5 o ö6

--- Ivana_Mladenovic/Python/Övningar v51/Övning5v51.py
+++ Ivana_Mladenovic/Python/Övningar v51/Övning5v51.py
@@ -1,9 +1,9 @@
 tal=int(input("Ange ett heltal: "))
 # om talet är delbar med 5
 if (tal % 5 == 0):
-    print(tal, "är multipel av 5")
+    print(tal, "är en multipel av 5")
 # annars
 else:
-    print(tal, "är inte multipel av 5")
+    print(tal, "är inte en multipel av 5")
 import sys
 exit()
