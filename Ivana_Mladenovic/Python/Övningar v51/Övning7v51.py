4edb0b00d6c4dbf5d8923a680a6453d107660452
IvanaMladenovic89 <you@example.com>
2021-12-28 20:42:39 +0100

    ö7

---
+++ Ivana_Mladenovic/Python/Övningar v51/Övning7v51.py
@@ -0,0 +1,9 @@
+antal=int(input("Ange antal reella tal: "))
+produkt=1 # sätter värde till 1 då det ej går att multiplicera med 0 
+# med funktionen range() loopas koden igenom ett antal ggr, här det värde i variabel antal
+for x in range(antal):
+    tal=float(input("Ange ett reellt tal: "))
+    # för varje varv multipliceras ett nytt tal till variabeln produkt
+    produkt=produkt*tal
+# printar ut medelvärdet
+print("Produkten av numren är: ",produkt)
