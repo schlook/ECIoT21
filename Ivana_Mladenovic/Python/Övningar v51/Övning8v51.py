1cec554fd0e8d9d8fd5cbd539d1627fa1ed4260f
IvanaMladenovic89 <you@example.com>
2021-12-29 18:58:01 +0100

    ö8

---
+++ Ivana_Mladenovic/Python/Övningar v51/Övning8v51.py
@@ -0,0 +1,12 @@
+antal=int(input("Ange antalet värden: "))
+medelv=1 # sätter värdet till 1 då det ej går att multiplicera med 0
+# med funktionen range() loopas koden igenom ett antal ggr, här det värde i variabel antal
+for x in range(antal):
+    tal=float(input("Ange ett flyttal: "))
+    # för varje varv multipliceras ett nytt tal till variabeln medelv
+    medelv=medelv*tal
+import math
+#n=float(math.pow(medelv, 1/antal))
+n=float(medelv**(1/antal))
+# printar ut det geometriska medelvärdet
+print("Det geometriska medelvärdet är: ",n)
