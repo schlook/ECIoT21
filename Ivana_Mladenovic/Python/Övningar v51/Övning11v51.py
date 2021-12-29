1a84eb8cb5b5dfbe2c7ada1c2dc055a43e99786e
IvanaMladenovic89 <you@example.com>
2021-12-29 22:05:45 +0100

    ö11

---
+++ Ivana_Mladenovic/Python/Övningar v51/Övning11v51.py
@@ -0,0 +1,12 @@
+#for-loop loppar från 100-199
+for x in range(100, 200):
+    summa=0
+    # tal får nytt värde av x för varje varv
+    tal=x
+    # så länge tal inte blir 0
+    while tal !=0:
+        summa = summa + (tal % 10)
+        tal = tal//10 # delar med 10 och avrundar neråt
+    # om summa delat på 2 inte får någon rest då har talets siffror jämnt värde och då skrivs talet ut
+    if (summa % 2 == 0):
+        print(x)
--- Ivana_Mladenovic/Python/Övningar v51/Övning9v51.py
+++ Ivana_Mladenovic/Python/Övningar v51/Övning9v51.py
@@ -2,7 +2,7 @@ tal = int(input("Ange ett heltal: "))
 summa = 0
 # while-loopen körs tills talet blir 0
 while (tal != 0):
-    # i variabeln summa sparas summa + restvärde av tal%10
+    # i variabeln summa sparas summa + restvärde av tal%10, det blir sista siffran i det inmatade talet
     summa = summa + (tal % 10) 
     # tal delas med 10 och operatorn- // avrundar talet neråt 
     # tex om inmatat tal är 345 efter första varvet blir det 34,5 och avrundas till 34
