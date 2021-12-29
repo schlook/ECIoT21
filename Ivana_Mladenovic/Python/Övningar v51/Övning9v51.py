5fbf6a9b894890c52d50dd96742ad2bd7493cc2b
IvanaMladenovic89 <you@example.com>
2021-12-29 19:46:50 +0100

    ö9

---
+++ Ivana_Mladenovic/Python/Övningar v51/Övning9v51.py
@@ -0,0 +1,10 @@
+tal = int(input("Ange ett heltal: "))
+summa = 0
+# while-loopen körs tills talet blir 0
+while (tal != 0):
+    # i variabeln summa sparas summa + restvärde av tal%10
+    summa = summa + (tal % 10) 
+    # tal delas med 10 och operatorn- // avrundar talet neråt 
+    # tex om inmatat tal är 345 efter första varvet blir det 34,5 och avrundas till 34
+    tal = tal//10 
+print("Summan av talen är: ", summa)
